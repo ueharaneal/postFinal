fbb5162ae76c7b8f496c8645d01155d00bea3988
Neal Uehara <ueharaneal@gmail.com>
2023-12-16 18:23:04 -0800

    added new models

--- models/users.js
+++ models/users.js
@@ -1,9 +1,46 @@
 import mongoose from 'mongoose'
+const validator = require('validator')
+
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-    googleId: String,
-    name: {name: String}
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+          if (!validator.isEmail(value)) {
+            throw new Error("Invalid Email");
+          }
+        },
+      },
+      password: {
+        type: String,
+        required: true, 
+        trim: true,
+        lowercase: true,
+      },
+      role: {
+        type: String,
+        enum:['user','admin'],
+        default: 'user' 
+      },
+      firstname: {
+        type: String,
+        trim: true,
+        maxLength: 100,
+      },
+      lastname: {
+        type: String,
+        trim: true,
+        maxLength: 100,
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      },
 });
 
 
