ddd7ba7e6d0f752364bfcf6a1340bc1ec275f2a4
Neal Uehara <ueharaneal@gmail.com>
2023-12-19 18:45:59 -0800

    notes

--- models/postSchema.js
+++ models/postSchema.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 
 
 const postSchema = new mongoose.Schema({
+    //once the indexes are in  sync remove the indexs from this schema 
+    //create a function that increments the indexs
+    //the indexes should increment independently
     postId: {type: String},
     volCodeArray: [[{type: Number}]],
     volCodeIndexZero: {type: Number},
