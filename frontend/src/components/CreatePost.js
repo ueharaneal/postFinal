0887a06194f18d8d9385b3f019d6852a3cee17f8
ueharaneal <ueharaneal@gmail.com>
2024-01-19 00:07:59 -0800

    formik

--- frontend/src/components/CreatePost.js
+++ frontend/src/components/CreatePost.js
@@ -6,14 +6,32 @@ function CreatePost() {
 		initialValues: {
 			postLocation: "",
 			postId: 0,
+			userName: "",
+			email: "",
 		},
 		onSubmit: values => {
-			alert()
+			alert(JSON.stringify(values, null, 2))
+		},
+		validate: values => {
+			let errors = {}
+			if (!values.email) {
+				errors.email = "Email is required"
+			} else if (
+				!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+			) {
+				errors.email = "Email is invalid"
+			}
+			if (!values.postLocation) {
+				errors.postLocation = "Post Location is required"
+			}
+			if (!values.postId) {
+				errors.postId = "Post id is required"
+			}
 		},
 	})
 	return (
 		//containter div that will also contain the for
-		<div>Create Post</div>
+		<div>C</div>
 	)
 }
 
