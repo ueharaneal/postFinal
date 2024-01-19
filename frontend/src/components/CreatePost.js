1e0ff64ececadc7a702f267b49301c92f5da19ee
ueharaneal <ueharaneal@gmail.com>
2024-01-19 00:59:05 -0800

    formik validation

--- frontend/src/components/CreatePost.js
+++ frontend/src/components/CreatePost.js
@@ -4,13 +4,13 @@ import Yup from "yup"
 function CreatePost() {
 	const formik = useFormik({
 		initialValues: {
-			postLocation: "",
-			postId: 0,
 			userName: "",
 			email: "",
+			postLocation: "",
+			postId: 0,
 		},
 		onSubmit: values => {
-			alert(JSON.stringify(values, null, 2))
+			alert(JSON.stringify(values, null, 3))
 		},
 		validate: values => {
 			let errors = {}
@@ -21,17 +21,69 @@ function CreatePost() {
 			) {
 				errors.email = "Email is invalid"
 			}
+			if (!values.userName) {
+				errors.userName = "Username is required"
+			}
 			if (!values.postLocation) {
 				errors.postLocation = "Post Location is required"
 			}
 			if (!values.postId) {
 				errors.postId = "Post id is required"
 			}
+			return errors
 		},
 	})
 	return (
 		//containter div that will also contain the for
-		<div>C</div>
+		<div>
+			<form onSubmit={formik.handleSubmit}>
+				<label htmlFor='email'>Email: </label>
+				<input
+					name='email'
+					id='email'
+					type='email'
+					onChange={formik.handleChange}
+					onBlur={formik.handleBlur}
+					value={formik.values.email}
+				></input>
+				{formik.touched && formik.errors.email ? (
+					<div>{formik.errors.email}</div>
+				) : null}
+				<label htmlFor='username'>Username: </label>
+				<input
+					name='userName'
+					id='userName'
+					type='text'
+					onChange={formik.handleChange}
+					onBlur={formik.handleBlur}
+					value={formik.values.userName}
+				></input>
+				{formik.touched && formik.errors.userName ? (
+					<span>{formik.errors.userName}</span>
+				) : null}
+
+				<label htmlFor='postLocation'>Post Location</label>
+				<input
+					name='postLocation'
+					id='postLocation'
+					type='text'
+					onChange={formik.handleChange}
+					onBlur={formik.handleBlur}
+					value={formik.values.postLocation}
+				></input>
+
+				<label htmlFor='postId'>Post ID</label>
+				<input
+					name='postId'
+					id='postId'
+					type='text'
+					onChange={formik.handleChange}
+					onBlur={formik.onBlur}
+					value={formik.values.postId}
+				></input>
+				<button type='submit'>Submit</button>
+			</form>
+		</div>
 	)
 }
 
