823f680ab67b7e19ff26b17552c3b38ddeb995af
ueharaneal <ueharaneal@gmail.com>
2024-02-01 04:50:10 -0800

    Created Routes

--- routes/router.js
+++ routes/router.js
@@ -49,7 +49,7 @@ router.post("/user/login", async (req, res, next) => {
 
 //Post Routes
 //to get all post data
-router.get("/api/postdata", async (req, res) => {
+router.get("/postdata", async (req, res) => {
   try {
     const allPosts = await posts.find(); // Use Mongoose to find all posts in the database
     res.json(allPosts); // Send the retrieved data as JSON response
--- server/server.js
+++ server/server.js
@@ -1,30 +1,30 @@
-const express = require("express");
-require("dotenv").config();
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const express = require("express")
+require("dotenv").config()
+const bodyParser = require("body-parser")
+const cors = require("cors")
 
-const app = express();
-const port = process.env.PORT || 3001;
-const router = require("../routes/router.js");
-const connectDb = require("../models/db.js");
+const app = express()
+const port = process.env.PORT || 3001
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+const routes = require("../routes/index.js")
+const connectDb = require("../models/db.js")
 
 const corsOptions = {
-  origin: "*",
-  credentials: true,
-  optionsSuccessState: 200,
-};
+	origin: "*",
+	credentials: true,
+	optionsSuccessState: 200,
+}
 
-connectDb();
+connectDb()
 
-app.use(cors(corsOptions));
+app.use(cors(corsOptions))
 
 //open database connection when application starts
 
-app.use("/", router);
+app.use("/api", routes)
 
 app.listen(port, () => {
-  console.log(`App is listening on port ${port}`);
-});
+	console.log(`App is listening on port ${port}`)
+})
