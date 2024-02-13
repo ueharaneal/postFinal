aea639aa4e6f09ddaea1964bb11cebe43352c7e6
ueharaneal <ueharaneal@gmail.com>
2024-02-13 04:54:35 -0800

    admin page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,29 @@
-import { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import SignInPage from "./pages/SignInPage";
-import AboutPage from "./pages/AboutPage";
-import FindPostPage from './pages/FindPostPage'
+import { useState } from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import SignInPage from "./pages/SignInPage"
+import AboutPage from "./pages/AboutPage"
+import FindPostPage from "./pages/FindPostPage"
+import AdminPage from "./pages/AdminPage"
 
-import NavBar from "./components/NavBar";
+import NavBar from "./components/NavBar"
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path='/about' element={<AboutPage/>} />
-          <Route path='/findapost' element={<FindPostPage/>}/>
-          <Route path="/signin" element={<SignInPage />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+	return (
+		<>
+			<BrowserRouter>
+				<NavBar />
+				<Routes>
+					<Route path='/' element={<HomePage />} />
+					<Route path='/home' element={<HomePage />} />
+					<Route path='/about' element={<AboutPage />} />
+					<Route path='/findapost' element={<FindPostPage />} />
+					<Route path='/signin' element={<SignInPage />}></Route>
+					<Route path='/admin' element={<AdminPage />}></Route>
+				</Routes>
+			</BrowserRouter>
+		</>
+	)
 }
 
-export default App;
+export default App
---
+++ frontend/src/pages/AdminPage.jsx
@@ -0,0 +1,9 @@
+function AdminPage(){
+    return(
+        <div>
+            This is the adminPage
+        </div>
+    )
+}
+
+export default AdminPage
