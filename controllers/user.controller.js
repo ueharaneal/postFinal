74e800a6097e11d188bba2df355fdabc5dac5f96
Neal Uehara <ueharaneal@gmail.com>
2024-02-17 23:36:22 -0800

    Fixed get first and last name request

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,15 +1,16 @@
-const {userServices}  = require('../services/user.service')
-const httpStatus = require('http-status')
+const { userService } = require("../services");
+const httpStatus = require("http-status");
 
 const userController = {
-    async getUsersNames(req,res,next){
-        try{
-            const userNames = await userServices.getUsersNames();
-            res.status(httpStatus.OK).send(userNames)
-        }catch(error){
-            res.status(httpStatus.BAD_REQUEST).send(error.message)
-        }
+  async getUsersNames(req, res, next) {
+    try {
+      const userNames = await userService.getUsersNames();
+      console.log("getUsersName got called");
+      res.status(httpStatus.OK).send(userNames);
+    } catch (error) {
+      res.status(httpStatus.BAD_REQUEST).send(error.message);
     }
-}
+  },
+};
 
-module.exports = userController
+module.exports = userController;
