fc0d1942a70596e429999afe7eca987f02c0db81
Neal Uehara <ueharaneal@gmail.com>
2024-02-18 17:10:34 -0800

    added sigIn page

--- frontend/src/components/customUi/RegisterForm.jsx
+++ frontend/src/components/customUi/RegisterForm.jsx
@@ -1,6 +1,5 @@
 import { motion } from "framer-motion";
 import { FiArrowUpRight, FiStar } from "react-icons/fi";
-import Singer from "../../assets/Singer.png";
 
 //fetching data
 import axios from "axios";
@@ -24,12 +23,7 @@ const validationSchema = Yup.object({
 });
 
 const RegisterForm = () => {
-  return (
-    <section className="grid min-h-screen grid-cols-1 bg-slate-50 md:grid-cols-[1fr,_400px] lg:grid-cols-[1fr,_600px]">
-      <RegisterFormik />
-      <SupplementalContent />
-    </section>
-  );
+  return <RegisterFormik />;
 };
 export default RegisterForm;
 const RegisterFormik = () => {
@@ -44,7 +38,7 @@ const RegisterFormik = () => {
       console.log(response);
       toast.success("Account Created!");
     } catch (error) {
-      console.log("there was an error from toaster")
+      console.log("there was an error from toaster");
       console.log(error.response?.data);
       if (error.response && error.response.data) {
         const errorMessage = error.response.data.message; // Assuming error message is in error.response.data.message
@@ -236,101 +230,6 @@ const RegisterFormik = () => {
       </div>
       <Toaster position="top-right" richColors />
     </motion.div>
-  );
-};
-
-const SupplementalContent = () => {
-  return (
-    <div className="group sticky top-4 m-4 h-60 overflow-hidden rounded-3xl rounded-tl-[4rem] bg-slate-950 md:h-[calc(100vh_-_15rem)]">
-      <img
-        alt="An example image"
-        src={Singer}
-        className="h-full w-full bg-white object-cover transition-all duration-500 group-hover:scale-105 group-hover:opacity-50"
-      />
-
-      <div className="absolute right-2 top-4 z-10">
-        <FiArrowUpRight className="rotate-45 text-6xl text-indigo-200 opacity-0 transition-all duration-500 group-hover:rotate-0 group-hover:opacity-100" />
-      </div>
-
-      <motion.div
-        initial="initial"
-        whileInView="animate"
-        transition={{
-          staggerChildren: 0.05,
-        }}
-        viewport={{ once: true }}
-        className="absolute inset-0 flex flex-col items-start justify-end bg-gradient-to-t from-slate-950/90 to-slate-950/0 p-8"
-      >
-        <motion.h2
-          className="mb-2 text-3xl font-semibold leading-[1.25] text-white lg:text-4xl"
-          variants={primaryVariants}
-        >
-          Connecting Musicians
-          <br />
-          with Opportunities
-        </motion.h2>
-        <motion.p
-          variants={primaryVariants}
-          className="mb-6 max-w-md text-sm text-slate-300"
-        >
-          Street Sound Society's innovative smart speakers are revolutionizing
-          the music industry by connecting aspiring artists with the public more
-          rapidly than ever before. Now, you can showcase your talent in public
-          spaces today, effortlessly bringing your music to life for audiences
-          everywhere!
-        </motion.p>
-        <div className="flex items-center gap-4">
-          <div className="flex items-center">
-            <motion.img
-              variants={avatarVariants}
-              className="h-8 w-8 rounded-full border-[1px] border-slate-100 object-cover shadow-inner"
-              alt="A placeholder testimonial image"
-              src="/imgs/head-shots/1.jpg"
-            />
-            <motion.img
-              variants={avatarVariants}
-              className="-ml-4 h-8 w-8 rounded-full border-[1px] border-slate-100 object-cover shadow-inner"
-              alt="A placeholder testimonial image"
-              src="/imgs/head-shots/2.jpg"
-            />
-            <motion.img
-              variants={avatarVariants}
-              className="-ml-4 h-8 w-8 rounded-full border-[1px] border-slate-100 object-cover shadow-inner"
-              alt="A placeholder testimonial image"
-              src="/imgs/head-shots/3.jpg"
-            />
-            <motion.img
-              variants={avatarVariants}
-              className="-ml-4 h-8 w-8 rounded-full border-[1px] border-slate-100 object-cover shadow-inner"
-              alt="A placeholder testimonial image"
-              src="/imgs/head-shots/4.jpg"
-            />
-            <motion.img
-              variants={avatarVariants}
-              className="-ml-4 h-8 w-8 rounded-full border-[1px] border-slate-100 object-cover shadow-inner"
-              alt="A placeholder testimonial image"
-              src="/imgs/head-shots/6.jpg"
-            />
-          </div>
-          <div>
-            <motion.div variants={primaryVariants} className="flex gap-0.5">
-              <FiStar className="fill-yellow-300 text-sm text-yellow-300" />
-              <FiStar className="fill-yellow-300 text-sm text-yellow-300" />
-              <FiStar className="fill-yellow-300 text-sm text-yellow-300" />
-              <FiStar className="fill-yellow-300 text-sm text-yellow-300" />
-              <FiStar className="fill-yellow-300 text-sm text-yellow-300" />
-              <span className="ml-2 text-sm text-white">5.0</span>
-            </motion.div>
-            <motion.p
-              variants={primaryVariants}
-              className="text-xs text-slate-300"
-            >
-              from over 100,000 reviews
-            </motion.p>
-          </div>
-        </div>
-      </motion.div>
-    </div>
   );
 };
 
--- frontend/src/pages/SignInPage.jsx
+++ frontend/src/pages/SignInPage.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import { useState } from "react";
 import Register from "../components/Register";
+import SignIn from "../components/SignIn";
+import SupplementalContent from "../components/customUi/SuplementalContent";
+
 function SignInPage() {
-  return (
-    <div>
-      <Register />
-    </div>
-  );
+  const [isRegisterPage, setIsRegisterPage] = useState(false);
+
+  const handleRegisterChange = ()=>{
+    setIsRegisterPage(!isRegisterPage)
+  }
+  return (<div>
+    <section className="grid min-h-screen grid-cols-1 bg-slate-50 md:grid-cols-[1fr,_400px] lg:grid-cols-[1fr,_600px]">
+      {isRegisterPage ? <Register /> : <SignIn />}
+      <SupplementalContent />
+    </section>
+    </div>);
 }
 
 export default SignInPage;
