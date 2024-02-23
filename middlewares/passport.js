f832638c7fecaf8ec1a324c6120f8e93b7d944c7
ueharaneal <ueharaneal@gmail.com>
2024-02-22 23:50:54 -0800

    jwt verification

--- middlewares/passport.js
+++ middlewares/passport.js
@@ -7,7 +7,7 @@ jwtOptions = {
 	secretOrKey: process.env.DB_SECRET,
 	jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
 }
-
+//function to verify that we have an existing token
 const jwtVerify = async () => {}
 
 const jwtStrategy = new Strategy()
